6caa3aa10c3287951083fc4d5bfcdcda9cb7264b
Zachary Delcoco <zdelcoco@dmsi.com>
2024-06-13 11:34:09 -0500

    Finished up with react-context, next section is using redux

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,42 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { useContext } from 'react';
+
+import MealsList from '../components/MealsList/MealsList';
+import { MEALS } from '../data/dummy-data';
+import { FavoritesContext } from '../store/context/favorites-context';
 
 function FavoritesScreen() {
-  return <Text>The favorites screen!</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter((meal) => {
+    return favoriteMealsCtx.isFavorite(meal.id);
+  });
+
+  if (favoriteMeals.length > 0) {
+    screen = (<MealsList items={favoriteMeals} />);
+  } else {
+    screen = (
+      <View style={styles.container}>
+        <Text style={styles.text}>No favorites yet!</Text>
+      </View>
+    );
+  }
+
+  return screen;
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
